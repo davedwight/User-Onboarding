e2ca73d006fb45e3ec92698bf8548a06958b9dc9
davedwight <dpdwight@gmail.com>
2021-05-20 16:04:37 -0600

    Added tests for all inputs

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -4,6 +4,8 @@ describe('Users App', () => {
     })
 
     const nameInput = () => cy.get('input[name=name]')
+    const emailInput = () => cy.get('input[name=email')
+    const passwordInput = () => cy.get('input[name=password')
 
     it('sanity check', () => {
         expect(1+2).to.equal(3)
@@ -14,6 +16,8 @@ describe('Users App', () => {
 
     it('the proper elements are showing', () => {
         nameInput().should('exist')
+        emailInput().should('exist')
+        passwordInput().should('exist')
     })
 
     describe('Filling out the inputs', () => {
@@ -26,6 +30,15 @@ describe('Users App', () => {
                 .should('have.value', '')
                 .type('Hello hello')
                 .should('have.value', 'Hello hello')
+            emailInput()
+                .should('have.value', '')
+                .type('hello@test.com')
+                .should('have.value', 'hello@test.com')
+            passwordInput()
+                .should('have.value', '')
+                .type('asdfASDF123')
+                .should('have.value', 'asdfASDF123')
+
         })
 
     })
